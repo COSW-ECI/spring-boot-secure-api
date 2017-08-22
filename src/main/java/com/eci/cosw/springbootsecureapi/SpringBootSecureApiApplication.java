a82bec7a1d9e9525d43788549be7176fd1bc4f99
Santiago Carrillo <sancarbar@gmail.com>
2017-08-21 21:30:45 -0500

    Added base project.

---
+++ src/main/java/com/eci/cosw/springbootsecureapi/SpringBootSecureApiApplication.java
@@ -0,0 +1,12 @@
+package com.eci.cosw.springbootsecureapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecureApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecureApiApplication.class, args);
+	}
+}
